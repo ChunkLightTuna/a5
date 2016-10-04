f0c799376427d88c825e0c6c0dc0d2c96bd5e0d0
ChunkLightTuna <chris@oeleri.ch>
2016-10-04 19:15:09 -0400

    k

--- src/main.kt
+++ src/main.kt
@@ -1,14 +1,8 @@
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.io.BufferedReader
 import java.io.FileInputStream
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.util.*
+import java.util.HashSet
 
 /**
- * Assignment 4: Graph Coloring
- *
- * wikipedia.org/wiki/Rapidly-exploring_random_tree
+ * Assignment 5: SAT Solver
  */
 
 fun main(args: Array<String>) {
@@ -25,56 +19,66 @@ fun main(args: Array<String>) {
     val reader = System.`in`.bufferedReader()
 
     var line = reader.readLine()
-    while (line.startsWith('c')) {
+
+    while (line[0] == 'c') {
         line = reader.readLine()
     }
-    val split = reader.readLine().trim().split(" ")
 
+    val split = line.trim().split(" ")
+
+    println("header! $line | var=${split[2].toInt()} cla=${split[3].toInt()}")
     val domain = Domain(numVariables = split[2].toInt(), numClauses = split[3].toInt())
 
     var i = reader.read()
-    val clause:MutableCollection<Int> = mutableListOf()
+    val clause: MutableCollection<Int> = mutableListOf()
+
     while (i != -1) {
         val ch = i.toChar()
-        if (ch == 'c') {
+
+        if (ch == 'c') {            //skip comments
             reader.readLine()
-        }
-        if (ch == '0') {
-            //next
+
+        } else if (ch == '0') {      //next
             domain.add(clause)
             clause.clear()
 
-        } else if (ch.isDigit()) {
-            //add
-            clause.add(ch.toInt())
+        } else if (ch == '-') {  //add
+            clause.add(Character.getNumericValue(reader.read().toChar()) * -1)
+
+        } else if (ch.isDigit()) {  //add
+            clause.add(Character.getNumericValue(ch))
         }
+
         i = reader.read()
     }
-
+    domain.assert()
 
     val sol = alg()
 
 }
 
-data class Three (var clause: Collection<Int>) {
 
-    fun contains(i: Int): Boolean {
-        return clause.contains(i)
-    }
-
-    fun containsN(i: Int): Boolean {
-        return clause.contains(-i)
-    }
-
-    fun remove(i: Int) {
-        clause = clause.minus(i)
-    }
-}
-
-
-class Domain(val numVariables: Int, val numClauses: Int, val clauses: HashSet<Collection<Int>> = hashSetOf()) {
+class Domain(val numVariables: Int, val numClauses: Int, val clauses: HashSet<Collection<Int>> = hashSetOf(), private val variables: HashSet<Int> = hashSetOf()) {
     fun add(clause: Collection<Int>) {
+        assert(clause.size > 0 && clause.size <= 3, { ->
+            "added clause of incorrect size. Expected 1-3, but found ${clause.size}"
+        })
         clauses.add(clause)
+        clause.forEach { variables.add(Math.abs(it)) }
+    }
+
+    fun assert() {
+        kotlin.assert(numClauses == clauses.size, { ->
+            "wrong # of clauses. Expected $numClauses, but found ${clauses.size}"
+        })
+        kotlin.assert(numVariables == variables.size, { ->
+            "wrong # of variables.  Expected $numVariables, but found ${variables.size}"
+        })
+
+        println("V Expected: $numVariables V Actual: ${variables.size}")
+        println("C Expected: $numClauses Actual: ${clauses.size}")
+
+        variables.clear()
     }
 }
 
